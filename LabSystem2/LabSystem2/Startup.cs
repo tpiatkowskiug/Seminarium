a349cb292f78a15956e45bf2a7ae6fdea5cd07bf
Tomasz <tpiatkowski@sigma.ug.edu.pl>
2019-04-29 21:22:19 +0200

    Początek projektu

---
+++ LabSystem2/LabSystem2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LabSystem2.Startup))]
+namespace LabSystem2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
