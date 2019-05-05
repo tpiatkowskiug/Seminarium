b64d6ad968e6064679021a7c8cd8fc05b09fd255
Tomasz <tpiatkowski@sigma.ug.edu.pl>
2019-05-06 00:03:12 +0200

    zmiany w shoppingCartManater

--- LabSystem2/LabSystem2/Infrastructure/ShoppingCartManager.cs
+++ LabSystem2/LabSystem2/Infrastructure/ShoppingCartManager.cs
@@ -106,7 +106,7 @@ namespace LabSystem2.Infrastructure
         {
             var cart = this.GetCart();
 
-            newOrder.OrderCreationDate = DateTime.Now;
+            newOrder.DateCreated = DateTime.Now;
             //newOrder.UserId = userId;
 
             this.db.Orders.Add(newOrder);
